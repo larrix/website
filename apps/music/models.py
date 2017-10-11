01611eccce1727e1570fd3f876144cb66108aa24
Luis Arrix <luis.arrix@gmail.com>
2017-10-11 12:22:24 -0300

    Septimo commit - Views genericas y forms

--- apps/music/models.py
+++ apps/music/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Album (models.Model):
@@ -7,6 +8,9 @@ class Album (models.Model):
     album_title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist

f42aa7a6fb2518e2759c6d0b3ecd1257168ff86b
Luis Arrix <luis.arrix@gmail.com>
2017-10-13 13:03:23 -0300

    Octavo commit - Subir archivos

--- apps/music/urls.py
+++ apps/music/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # /music/album/add
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),]
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/<album_id>
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/<album_id>/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+]
+
--- apps/music/views.py
+++ apps/music/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album
 
 
@@ -21,6 +22,15 @@ class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
 
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
 
 
 
